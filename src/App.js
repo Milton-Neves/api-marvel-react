e07af23ac5808f5ac831c6030ec7ee9b26b7febd
Milton Neves <milzonzneves@gmail.com>
2021-08-30 11:06:30 -0300

    Atualizar characters e organização de arquivos

--- src/App.js
+++ src/App.js
@@ -1,8 +1,9 @@
 import React from 'react';
 import Characters from './pages/Characters';
+import './App.css'
 
 export default function App() {
   return (
-    <Characters></Characters>
+       <Characters/>
   );
 }
--- src/pages/Characters/index.js
+++ src/pages/Characters/index.js
@@ -1,16 +1,27 @@
-import React, { useEffect } from 'react';
+import React, { useEffect, useState } from 'react';
 import api from "../../services/api";
 
-function Characters() {  
+function Characters() {
+
+  const [items, setItems] = useState([])
+  const [isLoading, setLoading] = useState(true)
+
   useEffect(() => {
-    api
-      .get(`/characters`,
-      )
-      .then(response => console.log(response.data.data))
-      .catch(err => {
-        console.log("ops! ocorreu um erro" + err);
-      });
+        await api
+            .get("/characters")
+            .then((response) => {
+              setItems(response.data.data.results);
+              setLoading(false);
+            })
+            .catch((err) => console.log(err));
+        
   }, []);
-  return <h1>characters</h1>
+
+  console.log(items);
+
+  return (
+    <div className="container">
+    </div>
+  );
 }
 export default Characters;
