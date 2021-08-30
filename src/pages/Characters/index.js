69e3bbb6462f6d784c7a64f6e1fddeb1da708abf
Milton Neves <milzonzneves@gmail.com>
2021-08-30 15:13:48 -0300

    Implementar o Search

--- src/pages/Characters/index.js
+++ src/pages/Characters/index.js
@@ -1,26 +1,50 @@
 import React, { useEffect, useState } from 'react';
 import api from "../../services/api";
+import CharacterTable from "./CharacterTable"
+import Search from './Search';
 
 function Characters() {
 
   const [items, setItems] = useState([])
   const [isLoading, setLoading] = useState(true)
+  const [search, setSearch] = useState('')
 
   useEffect(() => {
-        await api
+    async function onSearch() {
+      if (search === "") {
+        if (
+          localStorage.getItem("fav") === "[]" ||
+          !localStorage.getItem("fav")
+        ) {
+          await api
             .get("/characters")
             .then((response) => {
               setItems(response.data.data.results);
               setLoading(false);
             })
             .catch((err) => console.log(err));
-        
-  }, []);
+        } else {
+          const favor = JSON.parse(localStorage.getItem("favItem"));
+          setItems(favor);
+        }
+      } else {
+        await api
+          .get(`/characters?nameStartsWith=${search}`)
+          .then((response) => {
+            setItems(response.data.data.results);
+            setLoading(false);
+          })
+          .catch((err) => console.log(err));
+      }
+    }
+    onSearch();
+  }, [search]);
 
   console.log(items);
 
   return (
-    <div className="container">      
+    <div className="container">
+      <Search search={(q) => setSearch(q)}></Search>
       <CharacterTable items={items} isLoading={isLoading} />
     </div>
   );
